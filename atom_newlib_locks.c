8b1ae6843a460c0c433ee1a07a29f68c8a55050d
Andrew Dunstan <a_dunstan@hotmail.com>
2023-11-27 16:52:04 +1100

    silently skip the newlib retargetable locks if they're not needed

--- atom_newlib_locks.c
+++ atom_newlib_locks.c
@@ -2,9 +2,7 @@
 #include <stdlib.h>
 #include "atommutex.h"
 
-#if !defined(_RETARGETABLE_LOCKING) || (_RETARGETABLE_LOCKING == 0)
-#error "Newlib not compiled with retargetable lock functions"
-#endif
+#if defined(_RETARGETABLE_LOCKING)
 
 /* atomMutexCreate() simply clears all fields in the mutex struct,
  * so a statically allocated ATOM_MUTEX should already be zeroed / initialized
@@ -25,9 +23,7 @@ struct __lock __lock___dd_hash_mutex;
 struct __lock __lock___arc4random_mutex;
 
 void __retarget_lock_init(_LOCK_T* l) {
-	*l = (_LOCK_T)malloc(sizeof(**l));
-	if (*l != NULL)
-		atomMutexCreate(&((*l)->mutex));
+	__retarget_lock_init_recursive(l);
 }
 
 void __retarget_lock_init_recursive(_LOCK_T* l) {
@@ -37,10 +33,7 @@ void __retarget_lock_init_recursive(_LOCK_T* l) {
 }
 
 void __retarget_lock_close(_LOCK_T l) {
-	if (l != NULL) {
-		atomMutexDelete(&l->mutex);
-		free(l);
-	}
+	__retarget_lock_close_recursive(l);
 }
 
 void __retarget_lock_close_recursive(_LOCK_T l) {
@@ -51,7 +44,7 @@ void __retarget_lock_close_recursive(_LOCK_T l) {
 }
 
 void __retarget_lock_acquire(_LOCK_T l) {
-	atomMutexGet(&l->mutex, 0);
+	__retarget_lock_acquire_recursive(l);
 }
 
 void __retarget_lock_acquire_recursive(_LOCK_T l) {
@@ -59,21 +52,19 @@ void __retarget_lock_acquire_recursive(_LOCK_T l) {
 }
 
 int __retarget_lock_try_acquire(_LOCK_T l) {
-	if (atomMutexGet(&l->mutex, -1) == ATOM_OK)
-		return 1;
-	return 0;
+	return __retarget_lock_try_acquire_recursive(l);
 }
 
 int __retarget_lock_try_acquire_recursive(_LOCK_T l) {
-	if (atomMutexGet(&l->mutex, -1) == ATOM_OK)
-		return 1;
-	return 0;
+	return (atomMutexGet(&l->mutex, -1) == ATOM_OK) ? 1 : 0;
 }
 
 void __retarget_lock_release(_LOCK_T l) {
-	atomMutexPut(&l->mutex);
+	__retarget_lock_release_recursive(l);
 }
 
 void __retarget_lock_release_recursive(_LOCK_T l) {
 	atomMutexPut(&l->mutex);
 }
+
+#endif
