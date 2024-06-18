29326844d37a4a36208c003ba900f5c2e4518974
Andrew Dunstan <a_dunstan@hotmail.com>
2024-06-19 00:52:47 +1000

    fix library includes to be local

--- TeensyAtomThreads.h
+++ TeensyAtomThreads.h
@@ -1,5 +1,5 @@
-#include <atom.h>
-#include <atommutex.h>
-#include <atomqueue.h>
-#include <atomsem.h>
-#include <atomtimer.h>
+#include "atom.h"
+#include "atommutex.h"
+#include "atomqueue.h"
+#include "atomsem.h"
+#include "atomtimer.h"
